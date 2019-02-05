368b594a15cace336fc53ade8c9b9797883fd3ba
UnknownMonk <smodcartoon@gmail.com>
2019-02-05 10:13:33 -0500

    Added click events to social I cons

--- components/features.js
+++ components/features.js
@@ -22,4 +22,36 @@ let features = Array.from(document.querySelectorAll('.feature ')).map(
   f => new Feature(f)
 );
 
-window.addEventListener('mouseup', function(e) {}, false);
+const gitNoise = document.querySelector('.fa-github');
+
+gitNoise.addEventListener('click', sound_1);
+
+function sound_1() {
+  let audio = new Audio('audio/blip.wav');
+  audio.play();
+}
+const twitterNoise = document.querySelector('.fa-twitter');
+
+twitterNoise.addEventListener('click', sound_2);
+
+function sound_2() {
+  let audio1 = new Audio('audio/bloop_x.wav');
+  audio1.play();
+}
+
+const facebookNoise = document.querySelector('.fa-facebook-square');
+facebookNoise.addEventListener('click', sound_3);
+
+function sound_3() {
+  let audio2 = new Audio('audio/boo.wav');
+  audio2.play();
+}
+
+const instaNoise = document.querySelector('.fa-instagram');
+
+instaNoise.addEventListener('click', sound_4);
+
+function sound_4() {
+  let audio3 = new Audio('audio/bubbling1.wav');
+  audio3.play();
+}
