dbe9b3403d14b023dd0ef34ae334bb15a70a060e
UnknownMonk <smodcartoon@gmail.com>
2019-02-05 17:23:18 -0500

    fixed link

--- components/features.js
+++ components/features.js
@@ -1,3 +1,4 @@
+//Feature Component
 class Feature {
   constructor(element) {
     this.element = element;
@@ -21,41 +22,40 @@ class Feature {
 let features = Array.from(document.querySelectorAll('.feature ')).map(
   f => new Feature(f)
 );
-
+//Git Hub auio file
 const gitNoise = document.querySelector('.fa-github');
-
 gitNoise.addEventListener('click', sound_1);
-
+//Github audio function
 function sound_1() {
   let audio = new Audio('audio/blip.wav');
   audio.play();
 }
+//Twitter autio set up
 const twitterNoise = document.querySelector('.fa-twitter');
-
 twitterNoise.addEventListener('click', sound_2);
-
+//Twitter audio function
 function sound_2() {
   let audio1 = new Audio('audio/bloop_x.wav');
   audio1.play();
 }
-
+//Facebook audio setup
 const facebookNoise = document.querySelector('.fa-facebook-square');
 facebookNoise.addEventListener('click', sound_3);
-
+//Facebook audio function
 function sound_3() {
   let audio2 = new Audio('audio/boo.wav');
   audio2.play();
 }
-
+//Instagram audio setup
 const instaNoise = document.querySelector('.fa-instagram');
-
 instaNoise.addEventListener('click', sound_4);
-
+//Instagram audio function
 function sound_4() {
   let audio3 = new Audio('audio/bubbling1.wav');
   audio3.play();
 }
 
+//Adding DOM Element
 const addTip = document.querySelector('.box-2');
 
 addTip.addEventListener('click', heresATip);
